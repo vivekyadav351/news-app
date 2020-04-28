db05a84807d83f5d2a2f569f6e15d053119c15b2
VIVEK YADAV <VIVEKYADAV1412@GMAIL.COM>
2020-04-28 18:57:27 +0530

    Updates

---
+++ precache-manifest.60c3635115c128f4217d2f8addac51d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c4f4b82f80c01e6b69321e8c4162b3",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "1857cc5b187563f1d8d5",
+    "url": "/news-app/static/css/main.483697f4.chunk.css"
+  },
+  {
+    "revision": "539a7712548ab9dfe8a9",
+    "url": "/news-app/static/js/2.92697cb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-app/static/js/2.92697cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1857cc5b187563f1d8d5",
+    "url": "/news-app/static/js/main.4d8b7071.chunk.js"
+  },
+  {
+    "revision": "4d9a8010678fb1efbb80",
+    "url": "/news-app/static/js/runtime-main.0c15d165.js"
+  },
+  {
+    "revision": "2a93b0acbdfeb3479b1548481050139f",
+    "url": "/news-app/static/media/a2.2a93b0ac.png"
+  },
+  {
+    "revision": "e6ba88f58f0347dbe93a69108b1ce580",
+    "url": "/news-app/static/media/bang.e6ba88f5.jpeg"
+  },
+  {
+    "revision": "51af6882672f1b9905118eef4629eba1",
+    "url": "/news-app/static/media/del.51af6882.jpg"
+  },
+  {
+    "revision": "0719352b9cda5e120b711c7fbdf26073",
+    "url": "/news-app/static/media/hyd.0719352b.jpg"
+  },
+  {
+    "revision": "74b6fbe7f4afeafdf5da0da072d36def",
+    "url": "/news-app/static/media/mum.74b6fbe7.jpg"
+  }
+]);
